1e7081e14d6abfd5aaef9486dbb0d825f668fc8e
theQuery <tomkri2000@outlook.com>
2023-11-24 15:52:46 +0100

    Add optional gap to Section

--- src/components/Section/index.jsx
+++ src/components/Section/index.jsx
@@ -1,12 +1,15 @@
 import './index.css';
 
-function Section({ label, children }) {
+function Section({ label, gap, children }) {
   return <div className='section'>
     <h2 className='section__label'>
       {label}
       <span className='section__line'></span>
     </h2>
-    <div className='section__children'>
+    <div
+      className='section__children'
+      style={{ gap: gap ? '10px' : null }}
+    >
       {children}
     </div>
   </div>
