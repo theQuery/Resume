ce00ca67b736834616ea36f18939642f181e8098
theQuery <tomkri2000@outlook.com>
2023-11-25 22:03:27 +0100

    Add Projects component

---
+++ src/components/Projects/index.jsx
@@ -0,0 +1,46 @@
+import './index.css';
+import Block from '../Block';
+import Link from '../Link';
+
+function Tags({ tags }) {
+  return <div className='tags'>
+    {tags.map(tag => {
+      return <div key={tag} className='tags__tag'>
+        {tag}
+      </div>
+    })}
+  </div>
+}
+
+function Projects({ projects }) {
+  return <>
+    {projects.map(project => {
+      const { name, type, date, repository, deployment,
+        languages, tools, description } = project;
+
+      return <Block
+        key={name}
+        label={`${name} (${type})`}
+        date={date}
+      >
+        {description}
+        <div className='projects__details'>
+          <Link
+            link={deployment}
+            anchor='Deployment'
+            fallback='Ingen Deployment'
+          />
+          <Tags tags={languages} />
+          <Link
+            link={repository}
+            anchor='Repository'
+            fallback='Privat Repository'
+          />
+          <Tags tags={tools} />
+        </div>
+      </Block>
+    })}
+  </>
+}
+
+export default Projects;
