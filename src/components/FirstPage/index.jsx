a8ddcfd7e2fe5d8f6c9deac09a1f19caf96033c9
theQuery <tomkri2000@outlook.com>
2023-12-09 00:50:44 +0100

    Add FirstPage component

---
+++ src/components/FirstPage/index.jsx
@@ -0,0 +1,12 @@
+import Page from '../Page';
+import Aside from '../Aside';
+import Main from '../Main';
+
+function FirstPage() {
+  return <Page>
+    <Aside />
+    <Main />
+  </Page>
+}
+
+export default FirstPage;
