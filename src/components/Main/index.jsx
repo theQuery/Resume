88f2444e7e1943b91b7eaf5197f2f49a38c74463
theQuery <tomkri2000@outlook.com>
2023-12-09 01:08:58 +0100

    Add content to Main

--- src/components/Main/index.jsx
+++ src/components/Main/index.jsx
@@ -1,8 +1,30 @@
 import './index.css';
+import { about } from '../../assets/json/profile.json';
+import { highschool } from '../../assets/json/education.json';
+import largeProjects from '../../assets/json/projects-large.json';
+import Section from '../Section';
+import Block from '../Block';
+import Paragraph from '../Paragraph';
+import Projects from '../Projects';
 
 function Main() {
   return <main className='main'>
-    <h3>Prosjekter</h3>
+    <Section label='Profil' gap>
+      <Block label='Om'>
+        <Paragraph text={about} />
+      </Block>
+    </Section>
+    <Section label='Utdanning' gap>
+      <Block
+        label='Videregående IB Diplom'
+        date='Aug 2016 - Jun 2019'
+      >
+        <Paragraph text={highschool} />
+      </Block>
+    </Section>
+    <Section label='Prosjekter' gap>
+      <Projects projects={largeProjects} />
+    </Section>
   </main>
 }
 
